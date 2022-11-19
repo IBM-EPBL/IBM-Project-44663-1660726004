9a1f91d02e7153b051952a302e6acb909a909697
Kaviyanjali.P <113676163+Samanji@users.noreply.github.com>
2022-11-20 01:55:31 +0530

    Add files via upload

---
+++ FINAL DELIVARABLE/FINAL CODE.py
@@ -0,0 +1,68 @@
+
+				FINAL CODE
+
+import time
+import sys
+import ibmiotf.device
+import random
+
+
+#Provide your IBM Watson Device Credentials
+organization ="z2nb9k"
+deviceType = "raspberypi"
+deviceId = "123"
+authMethod = "token"
+authToken = "1234567890"
+
+# Initialize GPIO
+
+
+def myCommandCallback(cmd):
+    print("Command received: %s" % cmd.data['command'])
+    status=cmd.data['command']
+    if status=="Lighton":
+        print ("Light is on")
+    elif (status == "LIghtoff") :
+        print ("Light is off")
+    elif status == "sprinkleron":
+        print("Sprinkle is OFF")
+    elif status == "sprinkleron":
+        print("Sprinkle is ON")
+    #print(cmd)
+    
+        
+
+
+try:
+	deviceOptions = {"org": organization, "type": deviceType, "id": deviceId, "auth-method": authMethod, "auth-token": authToken}
+	deviceCli = ibmiotf.device.Client(deviceOptions)
+	#..............................................
+	
+except Exception as e:
+	print("Caught exception connecting device: %s" % str(e))
+	sys.exit()
+
+# Connect and send a datapoint "hello" with value "world" into the cloud as an event of type "greeting" 10 times
+deviceCli.connect()
+
+while True:
+        #Get Sensor Data from DHT11
+        
+        temp=random.randint(0,100)
+        Humid=random.randint(0,100)
+        gas=random.randint(0,100)
+        
+        data = { 'temp' : temp, 'Humid': Humid, 'gas' : gas }
+        #print data
+        def myOnPublishCallback():
+            print ("Published Temperature = %s C" % temp, "Humidity = %s %%" % Humid, "Gas_Level = %s %%" %gas, "to IBM Watson")
+
+        success = deviceCli.publishEvent("IoTSensor", "json", data, qos=0, on_publish=myOnPublishCallback)
+        if not success:
+            print("Not connected to IoTF")
+        time.sleep(1)
+        
+        deviceCli.commandCallback = myCommandCallback
+
+# Disconnect the device and application from the cloud
+deviceCli.disconnect()
